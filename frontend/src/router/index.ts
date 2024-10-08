1b5f3b9f8b8691e84432d5e3b539b30d6cf67586
MainSkown <mainskown.contact@gmail.com>
2024-10-08 14:41:56 +0200

    Basic page components

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,8 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Home from '@/components/MainPage.vue'
+import '../assets/main.css'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: []
+  routes: [
+    {path: '/', component: Home},
+  ]
 })
 
 export default router
