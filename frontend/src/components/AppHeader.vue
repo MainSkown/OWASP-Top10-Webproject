1bde38ff35d49fe22dd6f8ff263a1220ea2af7e2
MainSkown <mainskown.contact@gmail.com>
2024-10-15 21:59:05 +0200

    Too much fun with creating header

--- frontend/src/components/AppHeader.vue
+++ frontend/src/components/AppHeader.vue
@@ -8,16 +8,18 @@
       <h1>CatWorld</h1>
     </div>
     <div class="right-header">
-      
+      <Avatar :hash="'username'" :size="50" />
     </div>
   </div>
 </template>
 
 <script lang="ts">
-
+import Avatar from './Avatar.vue'
 export default {
   name: 'AppHeader',
-  
+  components: {
+    Avatar
+  }
 }
 </script>
 
@@ -25,17 +27,18 @@ export default {
 .header {
   background-color: #f3f3f3;
   display: flex;
-  justify-content: start;
+  flex-direction: row;
+  justify-content: space-between;
   width: 100vw;
-  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.1);
+  box-shadow: 0 2px 4px 0 #4612127a;
+  background: linear-gradient(to bottom, #f3f3f3 90%, #4612127a);
 }
 
 .left-header {
   display: flex;
   justify-content: space-between;
   align-items: center;
-  background-color: #f3f3f3;
-  padding-left: 10px;
+  padding-left: 20px;
   h1 {
     background: linear-gradient(to top right, #461212, #f01763);
     -webkit-background-clip: text;
@@ -53,5 +56,11 @@ export default {
   mask: url('/icon.svg') no-repeat center;
   -webkit-mask-size: cover;
   mask-size: cover;
+}
+
+.right-header {
+  display: flex;
+  align-items: center;
+  padding-right: 20px;
 }
 </style>
