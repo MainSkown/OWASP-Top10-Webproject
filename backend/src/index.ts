6d501118d15bc05974540d03ef1bbfa5a6527e60
MainSkown <mainskown.contact@gmail.com>
2024-11-11 19:01:51 +0100

    Simple backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,24 @@
 import express, {Express, Request, Response} from 'express'
 import dotenv from 'dotenv'
+import cors from 'cors'
 
 dotenv.config()
 
 const app: Express = express()
-const port = process.env.PORT || 3003
+const port = process.env.PORT || 3001
+
+const route = process.env.DEFAULT_ROUTE || "/api"
+
+app.use(cors())
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Hello World")
 });
 
+app.get(`${route}`, (req: Request, res: Response) => {
+    res.send("Hello World")
+})
+
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
+    console.log(`Server is running on port ${port}${route}`)
 });
